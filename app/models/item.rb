51cea214aad8f80647a0fcdc97524907bbc5de31
murakami-m <senazuki0825@yahoo.co.jp>
2022-01-06 19:24:27 +0900

    販売価格のバリデーションを追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,7 +16,7 @@ class Item < ApplicationRecord
     validates :delivery_charge_id, numericality: { other_than: 1 }
     validates :prefecture_id, numericality: { other_than: 1 }
     validates :days_to_ship_id, numericality: { other_than: 1 }
-    validates :pryce
+    validates :pryce, numericality: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/ }
     validates :user_id
   end
 end
