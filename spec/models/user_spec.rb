cfd5ab679bf3fe5bea61861d839089cba35c6ee5
murakami-m <senazuki0825@yahoo.co.jp>
2022-01-06 13:41:52 +0900

    テストコードの追記

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -57,6 +57,12 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is invalid')
       end
+      it 'passwordに全角文字が含まれると登録できない' do
+        @user.password = 'あbc123'
+        @user.password_confirmation = 'あbc123'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is invalid')
+      end
       it 'passwordとpassword_confirmationが一致しないと登録できない' do
         @user.password_confirmation = ''
         @user.valid?
