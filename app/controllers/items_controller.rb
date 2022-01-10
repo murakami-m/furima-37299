56545db42e56755a4ba48738f49cf57070f85452
murakami-m <senazuki0825@yahoo.co.jp>
2022-01-10 11:56:57 +0900

    before_actionの重複部分を修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, only: [:edit, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :move_to_index, only: [:edit, :destroy]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -46,12 +46,12 @@ class ItemsController < ApplicationController
                                  :days_to_ship_id, :price).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    @item = Item.find(params[:id])
-    redirect_to root_path unless @item.user_id == current_user.id
-  end
-
   def set_item
     @item = Item.find(params[:id])
   end
+  
+  def move_to_index
+    redirect_to root_path unless @item.user_id == current_user.id
+  end
+
 end
